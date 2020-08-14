d01aeea37b524388b0bd44ee2151410cd20b0d3a
Vishalcj17 <vishalcj@aospa.co>
2020-08-13 21:57:50 -0300

    lmi: Initial makefiles
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/lmi
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := lmi
+
+# Kernel
+BOARD_KERNEL_BASE := 0x0000
+BOARD_RAMDISK_OFFSET := 0x1000000
+TARGET_KERNEL_CONFIG := vendor/pa_lmi_defconfig
+
+# Platform
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno650
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/lmi/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/xiaomi/lmi/lmi-vendor.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
